f802b93a441d20846bcb9c6f3706ae775dca01b3
Joe Ng'ethe <>
2019-07-16 16:42:50 +0300

    mock data

---
+++ api/data.js
@@ -0,0 +1,5 @@
+let employees = [{ id: 1, name: 'Joe Doe', employerId: 2 }, { id: 2, name: 'Jane Doe', employerId: 1 }];
+
+let employers = [{ id: 1, name: 'Apple' }, { id: 2, name: 'Google' }];
+
+module.exports = { employees, employers };
